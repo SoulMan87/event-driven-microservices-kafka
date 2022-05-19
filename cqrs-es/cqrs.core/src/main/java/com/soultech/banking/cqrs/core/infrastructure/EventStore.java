a09aef73c5d95ee38b90b84a6fbeddfe5ed8eb8c
jonathan <jonathanhinestroza87@gmail.com>
2022-05-19 12:19:46 -0500

    events repositories

---
+++ cqrs-es/cqrs.core/src/main/java/com/soultech/banking/cqrs/core/infrastructure/EventStore.java
@@ -0,0 +1,12 @@
+package com.soultech.banking.cqrs.core.infrastructure;
+
+import com.soultech.banking.cqrs.core.events.BaseEvent;
+
+import java.util.List;
+
+public interface EventStore {
+
+    void saveEvents(String aggregateId, Iterable<BaseEvent> events, int expectedVersion);
+
+    List<BaseEvent> getEvent(String aggregateId);
+}
