aeb6ecfdb6390aa4e8ee1f96b04ed547538ef9f4
jonathan <jonathanhinestroza87@gmail.com>
2022-06-28 14:11:09 -0500

    event

--- banking-account/account.cmd/src/main/java/com/soultech/banking/cmd/CommandApplication.java
+++ banking-account/account.cmd/src/main/java/com/soultech/banking/cmd/CommandApplication.java
@@ -1,13 +1,32 @@
 package com.soultech.banking.cmd;
 
+import com.soultech.banking.cmd.api.command.*;
+import com.soultech.banking.cqrs.core.infrastructure.CommandDispatcher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class CommandApplication {
+
+    private final CommandDispatcher commandDispatcher;
+
+    private final CommandHandler commandHandler;
 
     public static void main(String[] args) {
         SpringApplication.run(CommandApplication.class, args);
+    }
+
+    @PostConstruct
+    public void registerHandler(){
+        commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(DepositFundsCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(WithdrawnFundsCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
     }
 
 }
